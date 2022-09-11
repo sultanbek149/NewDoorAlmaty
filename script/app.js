738a1b8f2b7c6556ddfba0f1314bc8b2c0419dee
Allaniyazov Sultanbek <allaniyazov.sultan@mail.ru>
2022-09-11 15:32:52 +0600

    yes

--- script/app.js
+++ script/app.js
@@ -11,3 +11,54 @@ const hideNavbarContent = () => {
 }
 
 navbarContent.addEventListener('click', hideNavbarContent)
+
+
+// Email form Handling
+console.log("Working")
+
+const emailForm = document.getElementById('emailForm');
+
+emailForm.addEventListener('submit', sendMail);
+
+function sendMail(e) {
+    e.preventDefault();
+
+    const fullName = document.getElementById('fname').value;
+
+    const pNumber = document.getElementById('pNumber').value;
+
+
+    Email.send({
+        SecureToken:
+            "74c06a47-6f01-4dd8-a8e3-8d5c5d81847f",
+        To: 'allaniyazovsultan1@gmail.com',
+        From: 'sultanhahahamaratov@gmail.com',
+        Subject: "NewDoorAlmaty",
+        Body: `Имя:             ${fullName}.<br>
+               Номер телефона:  ${pNumber}.`
+    }).then(() => showAlert("Ваше сообщение успешно отправлено!", true))
+        .catch((e) => showAlert(`Простите, сообщение не отправлено! ${e.message}`, false));
+
+    // sultanhahahamaratov@gmail.com
+    // password 80AC4DD439A67D3C6CDC2F9DB585070706C3
+
+    // abcdDCBA1234#
+    clearFields()
+}
+
+function showAlert(message, isSuccess) {
+    const div = document.createElement('div')
+    div.className = `alert ${isSuccess ? 'alert-success' : 'alert-danger'} alert-message`;
+    div.appendChild(document.createTextNode(message));
+
+    document.querySelector('.wrapper').appendChild(div);
+
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 4000);
+}
+
+function clearFields() {
+    document.querySelector('#pNumber').value = '';
+    document.querySelector('#fname').value = '';
+    document.querySelector('#fCheck').checked = false;
+}
